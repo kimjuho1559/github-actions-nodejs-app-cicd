899af12dd9f14d9f01883a680cd8d9cf91bc8b12
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-11-16 16:26:58 +0900

    commit2

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Hi, Server is running at http://${hostname}:${port}/`);
+  console.log(`Server is RUNning at http://${hostname}:${port}/`);
 });
 
 module.exports = server;
